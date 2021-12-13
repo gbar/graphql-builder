d2b6113737358b79ad14b0d196a77a755063f268
gbar <grzesiek_337@wp.pl>
2021-12-13 07:36:34 +0000

    fix: accept null values

--- GraphQLQueryBuilder.test.ts
+++ GraphQLQueryBuilder.test.ts
@@ -1,9 +1,9 @@
-import { GraphQLQueryBuilder } from './GraphQLQueryBuilder'
+import { GraphQLQueryBuilder } from './GraphQLQueryBuilder';
 
 describe('GraphQLQueryBuilder', () => {
   describe('Query requests', () => {
     it('should build query with nested fields', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const query = builder.createQuery({
         functionName: 'orders',
         fields: [
@@ -20,16 +20,16 @@ describe('GraphQLQueryBuilder', () => {
             ],
           },
         ],
-      })
+      });
 
       expect(query).toMatchObject({
         request: 'query { orders { id tax user { id name email address { city state } } } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build query with params', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const query = builder.createQuery({
         functionName: 'userLogin',
         fields: ['id', 'token'],
@@ -37,16 +37,16 @@ describe('GraphQLQueryBuilder', () => {
           email: 'jon.doe@example.com',
           password: 'pass',
         },
-      })
+      });
 
       expect(query).toMatchObject({
         request: 'query { userLogin(email: "jon.doe@example.com" password: "pass") { id token } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build query with nested operation', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const query = builder.createQuery([
         {
           functionName: 'someOperation',
@@ -59,16 +59,16 @@ describe('GraphQLQueryBuilder', () => {
           ],
           params: { id: 456 },
         },
-      ])
+      ]);
 
       expect(query).toMatchObject({
         request: 'query { someOperation(id: 456) { anotherOperation(id: 123) { field1 } } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build query with multiple operation without fields', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const query = builder.createQuery([
         {
           functionName: 'getFilteredCustomersCount',
@@ -85,16 +85,16 @@ describe('GraphQLQueryBuilder', () => {
             },
           ],
         },
-      ])
+      ]);
 
       expect(query).toMatchObject({
         request: 'query { getFilteredCustomersCount getAllCustomersCount getFilteredCustomers { count } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build query with multiple operation, complex params and alias', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const query = builder.createQuery([
         {
           key: 'alias1',
@@ -120,17 +120,17 @@ describe('GraphQLQueryBuilder', () => {
           fields: ['title'],
           params: { id: 'some-id' },
         },
-      ])
+      ]);
 
       expect(query).toMatchObject({
         request:
           'query { alias1: customer(identity: { sourceId: "sourceId1" sourceName: "latam" }) { id nonCanonicalProperties(names: [ "name1", "name2", "name3" ]) } alias2: customer(identity: { sourceId: "sourceId2" sourceName: "latam" }) { id } alias3: customer(identity: { sourceId: "sourceId3" sourceName: "latam" }) { id } alias4: customer(id: "some-id") { title } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build query with multiple operation, complex variables and alias', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const query = builder.createQuery([
         {
           key: 'alias1',
@@ -181,7 +181,7 @@ describe('GraphQLQueryBuilder', () => {
             },
           },
         },
-      ])
+      ]);
 
       expect(query).toMatchObject({
         request:
@@ -192,47 +192,47 @@ describe('GraphQLQueryBuilder', () => {
           identity3: { sourceId: 'sourceId3', sourceName: 'latam' },
           id1: 'some-id',
         },
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe('Mutation requests', () => {
     it('should build mutation with params', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const mutation = builder.createMutation({
         functionName: 'userRegister',
         fields: ['id'],
         params: { name: 'Jon Doe', email: 'jon.doe@example.com', password: 'password' },
-      })
+      });
 
       expect(mutation).toMatchObject({
         request: 'mutation { userRegister(name: "Jon Doe" email: "jon.doe@example.com" password: "password") { id } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build mutation with complex argument', async () => {
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const mutation = builder.createMutation({
         functionName: 'setPhone',
         params: {
           phone: { prefix: '+91', number: '9876543210' },
         },
         fields: ['id'],
-      })
+      });
 
       expect(mutation).toMatchObject({
         request: 'mutation { setPhone(phone: { prefix: "+91" number: "9876543210" }) { id } }',
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build mutation with multiple operation, complex params and alias', async () => {
-      const date = new Date()
-      const dateString = date.toISOString()
-      const fields = ['id']
+      const date = new Date();
+      const dateString = date.toISOString();
+      const fields = ['id'];
 
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const mutation = builder.createMutation([
         {
           key: 'alias1',
@@ -277,6 +277,7 @@ describe('GraphQLQueryBuilder', () => {
               id: 'internal-uuid4',
               data: {
                 gender: 'M',
+                dateOfBirth: null,
                 addresses: {
                   create: [
                     {
@@ -294,19 +295,19 @@ describe('GraphQLQueryBuilder', () => {
             },
           },
         },
-      ])
+      ]);
 
       expect(mutation).toMatchObject({
-        request: `mutation { alias1: createCustomer(input: { data: { firstName: "fname" lastName: "lname" dateOfBirth: "${dateString}" } source: { name: "latam" id: "external-id1" createdAt: "${dateString}" } }) { id } alias2: createCustomer(input: { data: { firstName: "fname" lastName: "lname" gender: "F" } source: { name: "latam" id: "external-id2" createdAt: "${dateString}" } }) { id } alias3: updateCustomer(input: { id: "internal-uuid3" data: { dateOfBirth: "${dateString}" } source: { name: "latam" updatedAt: "${dateString}" } }) { id } alias4: updateCustomer(input: { id: "internal-uuid4" data: { gender: "M" addresses: { create: [ { data: { lat: 1.23 lng: 3.123 } source: { name: "latam" id: "external-address-id1" createdAt: "${dateString}" } }, { data: { lat: 1.23 lng: 3.123 } source: { name: "latam" id: "external-address-id2" createdAt: "${dateString}" } } ] } } source: { name: "latam" updatedAt: "${dateString}" } }) { id } }`,
+        request: `mutation { alias1: createCustomer(input: { data: { firstName: "fname" lastName: "lname" dateOfBirth: "${dateString}" } source: { name: "latam" id: "external-id1" createdAt: "${dateString}" } }) { id } alias2: createCustomer(input: { data: { firstName: "fname" lastName: "lname" gender: "F" } source: { name: "latam" id: "external-id2" createdAt: "${dateString}" } }) { id } alias3: updateCustomer(input: { id: "internal-uuid3" data: { dateOfBirth: "${dateString}" } source: { name: "latam" updatedAt: "${dateString}" } }) { id } alias4: updateCustomer(input: { id: "internal-uuid4" data: { gender: "M" dateOfBirth: null addresses: { create: [ { data: { lat: 1.23 lng: 3.123 } source: { name: "latam" id: "external-address-id1" createdAt: "${dateString}" } }, { data: { lat: 1.23 lng: 3.123 } source: { name: "latam" id: "external-address-id2" createdAt: "${dateString}" } } ] } } source: { name: "latam" updatedAt: "${dateString}" } }) { id } }`,
         variables: {},
-      })
-    })
+      });
+    });
 
     it('should build mutation with multiple operation, complex variables and alias', async () => {
-      const date = new Date()
-      const fields = ['id']
+      const date = new Date();
+      const fields = ['id'];
 
-      const builder = new GraphQLQueryBuilder()
+      const builder = new GraphQLQueryBuilder();
       const mutation = builder.createMutation([
         {
           key: 'alias1',
@@ -364,13 +365,13 @@ describe('GraphQLQueryBuilder', () => {
               name: 'input',
               value: {
                 id: 'internal-uuid4',
-                data: { gender: 'M' },
+                data: { gender: 'M', dateOfBirth: null },
                 source: { name: 'latam', updatedAt: date },
               },
             },
           },
         },
-      ])
+      ]);
 
       expect(mutation).toMatchObject({
         request: `mutation($input1: CreateCustomerInput, $input2: CreateCustomerInput, $input3: UpdateCustomerInput, $input4: UpdateCustomerInput) { alias1: createCustomer(input: $input1) { id } alias2: createCustomer(input: $input2) { id } alias3: updateCustomer(input: $input3) { id } alias4: updateCustomer(input: $input4) { id } }`,
@@ -390,11 +391,11 @@ describe('GraphQLQueryBuilder', () => {
           },
           input4: {
             id: 'internal-uuid4',
-            data: { gender: 'M' },
+            data: { gender: 'M', dateOfBirth: null },
             source: { name: 'latam', updatedAt: date },
           },
         },
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
--- GraphQLQueryBuilder.ts
+++ GraphQLQueryBuilder.ts
@@ -1,4 +1,4 @@
-import { isEmpty, isNil } from 'ramda'
+import { isEmpty, isNil } from 'ramda';
 
 enum OperationType {
   Mutation = 'mutation',
@@ -6,185 +6,179 @@ enum OperationType {
   Subscription = 'subscription',
 }
 
-type ParamOption = string | number | boolean | Date | ParamOptions | Array<ParamOption>
+type ParamOption = string | number | boolean | Date | ParamOptions | Array<ParamOption>;
 
-type ParamOptions = { [key: string]: ParamOption }
+type ParamOptions = { [key: string]: ParamOption };
 
 type VariableOption = {
-  type: string
-  name: string
-  value: ParamOption
-  list?: boolean
-  required?: boolean
-}
+  type: string;
+  name: string;
+  value: ParamOption;
+  list?: boolean;
+  required?: boolean;
+};
 
-type VariableOptions = { [key: string]: VariableOption }
+type VariableOptions = { [key: string]: VariableOption };
 
-type NestedField = { [key: string]: Array<Field> }
+type NestedField = { [key: string]: Array<Field> };
 
-type Field = string | NestedField | OperationOptions
+type Field = string | NestedField | OperationOptions;
 
 export interface OperationOptions {
-  key?: string
-  functionName: string
-  fields?: Array<Field>
-  params?: ParamOptions
-  variables?: VariableOptions
+  key?: string;
+  functionName: string;
+  fields?: Array<Field>;
+  params?: ParamOptions;
+  variables?: VariableOptions;
 }
 
 const isOperation = (object): object is OperationOptions =>
-  typeof object === 'object' && object.hasOwnProperty('functionName')
+  typeof object === 'object' && object.hasOwnProperty('functionName');
 
-export type GraphQLRequest = { request: string; variables: ParamOptions }
+export type GraphQLRequest = { request: string; variables: ParamOptions };
 
 export class GraphQLQueryBuilder {
-  private variableDefinitions: Record<string, string> = {}
-  private variables: ParamOptions = {}
+  private variableDefinitions: Record<string, string> = {};
+  private variables: ParamOptions = {};
 
   private clear = () => {
-    this.variableDefinitions = {}
-    this.variables = {}
-  }
+    this.variableDefinitions = {};
+    this.variables = {};
+  };
 
   private buildFields = (fields: Array<Field>): string => {
     if (isEmpty(fields) || isNil(fields)) {
-      return ''
+      return '';
     }
 
     return ` { ${fields.reduce((query, field) => {
-      const _query = query ? `${query} ` : query
+      const _query = query ? `${query} ` : query;
 
       if (isOperation(field)) {
-        return `${_query}${this.buildOperation(field)}`
+        return `${_query}${this.buildOperation(field)}`;
       }
 
       if (typeof field === 'object' && !isEmpty(field)) {
         return `${_query}${Object.entries(field).reduce((subQuery, [key, value]) => {
-          const fields = this.buildFields(value)
+          const fields = this.buildFields(value);
 
-          return `${subQuery}${key}${fields}`
-        }, '')}`
+          return `${subQuery}${key}${fields}`;
+        }, '')}`;
       }
 
-      return `${_query}${field}`
-    }, '')} }`
-  }
+      return `${_query}${field}`;
+    }, '')} }`;
+  };
 
   private buildOperation = (options: OperationOptions | OperationOptions[], operationType?: OperationType): string => {
-    const _options = Array.isArray(options) ? options : [options]
+    const _options = Array.isArray(options) ? options : [options];
 
     const query = _options.reduce((query, option) => {
-      const _query = query ? `${query} ` : query
-      const key = option?.key ? `${option.key}: ` : ''
-      const variables = this.buildVariables(option?.variables)
-      const params = isEmpty(variables) ? this.buildParams(option?.params) : ''
-      const fields = this.buildFields(option?.fields)
+      const _query = query ? `${query} ` : query;
+      const key = option?.key ? `${option.key}: ` : '';
+      const variables = this.buildVariables(option?.variables);
+      const params = isEmpty(variables) ? this.buildParams(option?.params) : '';
+      const fields = this.buildFields(option?.fields);
 
-      return `${_query}${key}${option.functionName}${variables}${params}${fields}`
-    }, '')
+      return `${_query}${key}${option.functionName}${variables}${params}${fields}`;
+    }, '');
 
     if (isEmpty(query) || isNil(query)) {
-      return ''
+      return '';
     }
 
     if (isEmpty(operationType) || isNil(operationType)) {
-      return query
+      return query;
     }
 
     const definitions = !isEmpty(this.variableDefinitions)
       ? `(${Object.values(this.variableDefinitions).join(', ')})`
-      : ''
+      : '';
 
-    return `${operationType}${definitions} { ${query} }`
-  }
+    return `${operationType}${definitions} { ${query} }`;
+  };
 
   private resolveParamType = (param: { key?: string; option: ParamOption }): string => {
-    const { key, option } = param
-    const _key = key ? `${key}: ` : ''
+    const { key, option } = param;
+    const _key = key ? `${key}: ` : '';
 
-    if (typeof option === 'boolean' || typeof option === 'number') {
-      return `${_key}${option}`
+    if (typeof option === 'boolean' || typeof option === 'number' || isNil(option)) {
+      return `${_key}${option}`;
     }
 
     if (option instanceof Date) {
-      return `${_key}"${option.toISOString()}"`
+      return `${_key}"${option.toISOString()}"`;
     }
 
     if (Array.isArray(option)) {
-      return `${_key}${this.buildParams(option, { array: true })}`
+      return `${_key}${this.buildParams(option, { array: true })}`;
     }
 
-    if (typeof option === 'object') {
-      return `${_key}${this.buildParams(option, { object: true })}`
+    if (typeof option === 'object' && !isNil(option)) {
+      return `${_key}${this.buildParams(option, { object: true })}`;
     }
 
-    return `${_key}"${option}"`
-  }
+    return `${_key}"${option}"`;
+  };
 
   private buildParams = (
     options: ParamOptions | Array<ParamOption>,
-    nested?: { array?: boolean; object?: boolean },
+    nested?: { array?: boolean; object?: boolean }
   ): string => {
     if (isEmpty(options) || isNil(options)) {
-      return ''
+      return '';
     }
 
-    let output = ''
+    let output = '';
     if (Array.isArray(options)) {
-      output = `${options
-        .filter((option) => !isEmpty(option) && !isNil(option))
-        .map((option) => this.resolveParamType({ option }))
-        .join(', ')}`
+      output = `${options.map((option) => this.resolveParamType({ option })).join(', ')}`;
     } else {
       output = `${Object.entries(options)
-        .filter(([, option]) => !isEmpty(option) && !isNil(option))
         .map(([key, option]) => this.resolveParamType({ key, option }))
-        .join(' ')}`
+        .join(' ')}`;
     }
 
-    return nested?.object ? `{ ${output} }` : nested?.array ? `[ ${output} ]` : `(${output})`
-  }
+    return nested?.object ? `{ ${output} }` : nested?.array ? `[ ${output} ]` : `(${output})`;
+  };
 
   private buildVariables = (options: VariableOptions): string => {
     if (isEmpty(options) || isNil(options)) {
-      return ''
+      return '';
     }
 
-    const operationVariables: Array<string> = []
-    Object.entries(options)
-      .filter(([, option]) => !isEmpty(option.value) && !isNil(option.value))
-      .forEach(([key, option]) => {
-        const type = `${option.type}${option.required ? '!' : ''}`
-        const definition = `$${key}: ${option.list ? `[${type}]` : type}`
-        const operationVariable = `${option.name}: $${key}`
+    const operationVariables: Array<string> = [];
+    Object.entries(options).forEach(([key, option]) => {
+      const type = `${option.type}${option.required ? '!' : ''}`;
+      const definition = `$${key}: ${option.list ? `[${type}]` : type}`;
+      const operationVariable = `${option.name}: $${key}`;
 
-        this.variableDefinitions[key] = definition
-        this.variables[key] = option.value
-        operationVariables.push(operationVariable)
-      })
+      this.variableDefinitions[key] = definition;
+      this.variables[key] = option.value;
+      operationVariables.push(operationVariable);
+    });
 
-    return `(${operationVariables.join(', ')})`
-  }
+    return `(${operationVariables.join(', ')})`;
+  };
 
   createQuery = (options: OperationOptions | OperationOptions[]): GraphQLRequest | void => {
-    this.clear()
-    const request = this.buildOperation(options, OperationType.Query)
+    this.clear();
+    const request = this.buildOperation(options, OperationType.Query);
 
     if (isEmpty(request) || isNil(request)) {
-      return
+      return;
     }
 
-    return { request, variables: { ...this.variables } }
-  }
+    return { request, variables: { ...this.variables } };
+  };
 
   createMutation = (options: OperationOptions | OperationOptions[]): GraphQLRequest | void => {
-    this.clear()
-    const request = this.buildOperation(options, OperationType.Mutation)
+    this.clear();
+    const request = this.buildOperation(options, OperationType.Mutation);
 
     if (isEmpty(request) || isNil(request)) {
-      return
+      return;
     }
 
-    return { request, variables: { ...this.variables } }
-  }
+    return { request, variables: { ...this.variables } };
+  };
 }
